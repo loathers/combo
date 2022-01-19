beeec5b001c841e14ab21082d91046a2b288fa1d
horrible little slime <69secret69email69@gmail.com>
2022-01-19 12:27:43 -0500

    add prints

--- src/combo.ts
+++ src/combo.ts
@@ -4,10 +4,15 @@ import {
   fileToBuffer,
   gamedayToInt,
   handlingChoice,
+  isDarkMode,
   myId,
+  print,
   runChoice,
 } from "kolmafia";
 import { get, property, set, sinceKolmafiaRevision } from "libram";
+
+// Gotta print in a legible colour.
+const HIGHLIGHT = isDarkMode() ? "yellow" : "blue";
 
 type BeachTile = { minute: number; row: number; column: number };
 
@@ -53,6 +58,7 @@ function _comb(tile: BeachTile): void {
     // Mafia inexplicably indexes columns starting at 0, and rows starting at 1
     // This is beneficial to us! Arrays are indexed starting at 1, so .findIndex() gets us just what we want
     const whaleColumn = whaleRow[1].findIndex((x) => x === "W");
+    print("We found a whale. To hey deze's heart, we'll stab at it.", HIGHLIGHT);
     cliExecute(`beach comb ${whaleRow[0]} ${whaleColumn}`);
     // By returning partway through, we avoid needing to use "else"
     // With the exception of try...finally shenanigans, using the return keyword will just immediately terminate the function
@@ -63,6 +69,7 @@ function _comb(tile: BeachTile): void {
   if (rareRow) {
     // Here we make sure that the tile we were going to comb wasn't already combed
     if (rareRow[column] !== "c") {
+      print("Our rare tile is uncombed, so let's go ahead and change that.", HIGHLIGHT);
       cliExecute(`beach comb ${row} ${column}`);
       return;
     }
@@ -72,6 +79,7 @@ function _comb(tile: BeachTile): void {
     rowLayout[1].includes("t")
   );
   if (firstTwinkleRow) {
+    print("Our rare tile is combed, but we found a twinkle!", HIGHLIGHT);
     const twinkleColumn = firstTwinkleRow[1].findIndex((x) => x === "t");
     cliExecute(`beach comb ${firstTwinkleRow[0]} ${twinkleColumn}`);
     return;
